8badb5f0968c240d515cb5e8b85559668322924a
wdnei <wdneipaixao@gmail.com>
2015-07-08 01:45:24 -0300

    add datasource

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,5 +1,7 @@
 
-
+var storage_dir=null;
+if(process.env.OPENSHIFT_DATA_DIR)
+	storage_dir=process.env.OPENSHIFT_DATA_DIR+"/storage";
 
 module.exports = {
   "db": {
@@ -11,7 +13,7 @@ module.exports = {
     "host":process.env.OPENSHIFT_MYSQL_DB_HOST || "127.0.0.1",
     "port":process.env.OPENSHIFT_MYSQL_DB_PORT || 3307,
     "url":process.env.OPENSHIFT_MYSQL_DB_URL || "http://127.0.0.1:3307",
-    "database": "caretta",
+    "database": "carettalb",
     "password": process.env.OPENSHIFT_MYSQL_DB_PASSWORD || "usbw",
     "name": "mySQL",
     "connector": "mysql",
@@ -21,6 +23,6 @@ module.exports = {
     "name": "storage",
     "connector": "loopback-component-storage",
     "provider": "filesystem",
-    "root":process.env.OPENSHIFT_DATA_DIR || "./server/storage"
+    "root":storage_dir || "./server/storage"
   }
 }
